4ae5ed81b1c7b917cd26c705163844d0b449a2d8
wxwind <373903632@qq.com>
2023-11-27 00:36:58 +0800

    chore: more test

---
+++ DeerJson.Tests/Derialize/DeserializeUnicode.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace DeerJson.Tests;
+
+public class DeserializeUnicode
+{
+    private readonly JsonMapper m_jsonMapper = new();
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    // [Test]
+    // public void Unicode()
+    // {
+    //     var json = "\"\u0080\"";
+    //     var obj = m_jsonMapper.ParseJson<string>(json);
+    //     var expected = "°";
+    //
+    //     obj.Should().BeEquivalentTo(expected);
+    // }
+
+    [Test]
+    public void Unicode()
+    {
+        var json = "\"°\"";
+        var obj = m_jsonMapper.ParseJson<string>(json);
+        var expected = "°";
+
+        obj.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ DeerJson.Tests/Serialize/SerializeUnicode.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace DeerJson.Tests;
+
+public class SerializeUnicode
+{
+    private readonly JsonMapper m_jsonMapper = new();
+
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Unicode()
+    {
+        var expected = @"""°""";
+        var s = '°';
+        var json = m_jsonMapper.ToJson(s);
+
+        json.Should().Be(expected);
+    }
+}
